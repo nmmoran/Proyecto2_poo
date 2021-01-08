edbef18fb34c3233dc0e23aff5a4db43762d2512
Dustin Pisco <Dustin Pisco@LAPTOP-H7VV39BC>
2021-01-07 21:39:06 -0500

    Carpeta añadida y modic}ficacion de Login vista

--- src/main/java/com/pooespol/proyecto2_poo/App.java
+++ src/main/java/com/pooespol/proyecto2_poo/App.java
@@ -20,7 +20,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        scene = new Scene(loadFXML("administrador"), 640, 480);
+        scene = new Scene(loadFXML("administrador"), 1000, 700);
 
         stage.setScene(scene);
         stage.show();
