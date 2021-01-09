1940b0eab85b63e0828d8fc04110b9169687fba4
nmmoran <nmmoran@espol.edu.ec>
2021-01-08 23:26:17 -0500

    Update

--- src/main/java/com/pooespol/proyecto2_poo/data/ProductosData.java
+++ src/main/java/com/pooespol/proyecto2_poo/data/ProductosData.java
@@ -5,10 +5,32 @@
  */
 package com.pooespol.proyecto2_poo.data;
 
+import com.pooespol.proyecto2_poo.App;
+import com.pooespol.proyecto2_poo.modelo.Producto;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
 /**
  *
  * @author nicol
  */
 public class ProductosData {
+    static String ruta = "productos.txt";
     
+    /**
+     * Esta funcion lee el archivo productos.txt que se encuentra en 
+     * el paquete recursos y retorna un ArrayList con los generos descritos 
+     * en el archivo
+     * FORMATO ARCHIVO
+     *  tipo;producto;precio;imagen
+     * @return ArrayList<Producto>
+     */
+    public static ArrayList<Producto>leerProducto() throws IOException {
+        ArrayList<Producto> usu = new ArrayList<>();
+     }   
 }
